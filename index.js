ff128228cf33bf0b779f4d8d82289113b718cf4d
mdaslamalidu <thehometutor2022@gmail.com>
2023-01-09 01:03:14 +0600

    add customer data

--- index.js
+++ index.js
@@ -106,12 +106,10 @@ async function run() {
         _id: ObjectId(query.service),
       });
 
-      console.log(orderedService);
-
       const data = {
-        total_amount: 100,
-        currency: "BDT",
-        tran_id: "REF123", // use unique tran_id for each api call
+        total_amount: orderedService.price,
+        currency: query.currency,
+        tran_id: new ObjectId().toString(), // use unique tran_id for each api call
         success_url: "http://localhost:3030/success",
         fail_url: "http://localhost:3030/fail",
         cancel_url: "http://localhost:3030/cancel",
@@ -120,8 +118,8 @@ async function run() {
         product_name: "Computer.",
         product_category: "Electronic",
         product_profile: "general",
-        cus_name: "Customer Name",
-        cus_email: "customer@example.com",
+        cus_name: query.customer,
+        cus_email: query.email,
         cus_add1: "Dhaka",
         cus_add2: "Dhaka",
         cus_city: "Dhaka",
@@ -135,7 +133,7 @@ async function run() {
         ship_add2: "Dhaka",
         ship_city: "Dhaka",
         ship_state: "Dhaka",
-        ship_postcode: 1000,
+        ship_postcode: query.postcode,
         ship_country: "Bangladesh",
       };
 
